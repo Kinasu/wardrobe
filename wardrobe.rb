c44f974820e9e7ebc1b4e42c98558bfc83a57184
Kinasu <ashtoreet@gmail.com>
2017-02-03 15:34:28 +0300

    fix bugs

--- wardrobe.rb
+++ wardrobe.rb
@@ -5,27 +5,24 @@ class Wardrobe
   end
 
   def wear
-    @counter = 2
+    @counter = 3
     @line_counter = 0
     @clothes = []
+
     while @filelines.size > @counter
       @clothes << [@filelines[@line_counter], @filelines[@line_counter + 1], @filelines[@line_counter + 2]]
-
       @line_counter += 3
-      @counter += 2
+      @counter += 3
     end
   end
 
 
   def sky
-    @dress_count = 2
     @clothes.each do |dress|
-      @temp_range = dress[@dress_count].gsub(/[^\d,-]/, "").split(',').map{|d| Integer(d)}
+      @temp_range = dress[2].gsub(/[^\d,-]/, "").split(',').map{|d| Integer(d)}
       @range = @temp_range[0]..@temp_range[1]
-      @dress_count + 3
 
       puts dress if @range.include?(@weather)
     end
   end
-
 end
