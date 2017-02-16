aedee77bb2cb3ef3f3fd8848e140012beecfb6c6
Kinasu <ashtoreet@gmail.com>
2017-02-16 15:00:57 +0300

    remake+

--- main.rb
+++ main.rb
@@ -1,5 +1,6 @@
 require_relative "wardrobe.rb"
 
+#Путь файла
 path_data_files = File.dirname(__FILE__) + "/data/"
 files = Dir.entries(path_data_files)
 
@@ -18,20 +19,6 @@ for file in files
     goods << [filelines[0],filelines[1],filelines[2]]
   end
 end
-
-p goods
-# if File.exist? ("data/wear.txt")
-#   file = File.open("data/wear.txt", "r:UTF-8")
-#   @filelines = file.readlines
-#   file.close
-#   @filelines.map! {|s| s.gsub("\n", "")}
-# else
-#   puts "Файл не найден"
-# end
-
-# Программа спрашивает у пользователя, какая сейчас температура, а потом генерирует
-# подходящий набор одежды, по одной шмотке каждого типа. Например шапка из зайца,
-# меховая куртка, теплые джинсы, зимние ботинки.
 
 puts "Какая погода сейчас за окном?"
 weather = gets.to_i
