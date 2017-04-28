ed408547913a43e1b75b76747a8aaf0959d5dc59
Kinasu <ashtoreet@gmail.com>
2017-04-28 10:31:09 +0300

    refactoring tests

--- spec/clothes_item_spec.rb
+++ spec/clothes_item_spec.rb
@@ -6,11 +6,14 @@ describe ClothesItem do
   context 'Class clothes_item include information about things' do
     it 'initialize returns attribute of things' do
       expect(clothes_item.temperature_range).to eq -15..30
+      expect(clothes_item.type).to eq "брюки"
+      expect(clothes_item.name).to eq "джинсы"
     end
 
     it 'suitable? does the given number is in the range of numbers' do
-      expect(clothes_item.suitable?(5))
-        .to be true
+      expect(clothes_item.suitable?(5)).to be true
+      expect(clothes_item.suitable?(-20)).to be false
+
     end
   end
 end
